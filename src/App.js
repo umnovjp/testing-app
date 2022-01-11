1cba0812a0328167939dcd651fdc500de7e2e023
umnovjp <umnovjp@yahoo.com>
2022-01-10 21:16:51 -0800

    added initial files

--- src/App.js
+++ src/App.js
@@ -1,23 +1,38 @@
-import logo from './logo.svg';
-import './App.css';
+// We import useState and useEffect in our component
+import React, { useState, useEffect } from 'react';
+import SearchBar from './components/SearchBar';
+import IssueList from './components/IssueList';
 
 function App() {
+  // We declare a state variable that is an array called `issues` and a function to update it.
+  const [issues, setIssues] = useState([]);
+
+  // When the page loads, set the document title to something specific to this app.
+  // This only runs once because of our empty dependency array.
+  useEffect(() => {
+    document.title = 'GitHub issues';
+  }, []);
+
+  // Helper function that preforms an API request and sets the `issues` array to a list of issues from GitHub
+  const getRepoIssues = (repo) => {
+    let issuesURL = `https://api.github.com/repos/${repo}/issues?direction=asc`;
+    console.log('issuesURL', issuesURL);
+    fetch(issuesURL)
+      .then((res) => res.json())
+      .then((response) => setIssues(response));
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="ui container">
+      {/* Here we pass our getRepoIssues function as a prop to SearchBar */}
+      <SearchBar onFormSubmit={getRepoIssues} />
+      <div className="ui grid">
+        <div className="ui row">
+          <div className="eleven wide column">
+            <IssueList issues={issues} />
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
